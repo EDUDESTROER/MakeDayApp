9d735b50c2a972eca2f087cba6342befeb269a7a
EDUDESTROER <eduardopicholi@hotmail.com>
2024-03-02 10:38:36 -0300

    Adding click style to div calendar elements

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,63 @@
-window.viewControler = new ViewControler();
+window.viewControler = new View();
+window.noteControler = new NoteController();
+
+this._locale = navigator.language;
+this._usersMenu = document.querySelector('#user-menu-wrapper');
+this._wrapperCategories = document.querySelector('.wrapper-categories');
+this._calendarElement = document.querySelector('#calendar');
+this._searchBarElement = document.querySelector('#search-bar');
+this.initButtonEvents();
+
+
+
+function functionsBtn(value){
+
+    switch(value){
+
+        case 'menu-users':
+            viewControler.showElement(this._usersMenu);
+            break;
+        case 'menu-bar':
+            if(!this._wrapperCategories.style.display){
+                this._wrapperCategories.style.display = "flex";
+            }
+            viewControler.showElement(this._wrapperCategories);
+            break;
+        case 'menu-users-close':
+            viewControler.showElement(this._usersMenu);
+            break;
+        case 'calendar-up':
+            noteControler.changeMonth(this._calendarElement, this._locale, true);
+            break;
+        case 'calendar-down':
+            noteControler.changeMonth(this._calendarElement, this._locale, false);
+            break;    
+        case 'calendar':
+            noteControler.formatCalendar(this._calendarElement, this._locale, true);
+            viewControler.showElement(this._calendarElement);
+            break;
+        case 'search':
+            viewControler.showElement(this._searchBarElement);
+            break;
+        default:
+            console.error(value, ' Is not a valid button')
+            break;
+    }
+
+}
+ function initButtonEvents(){
+    let buttons = document.querySelectorAll('button');
+
+    buttons.forEach(btn=>{
+        btn.addEventListener('click', e=>{
+
+            let textBtn = btn.id.replace("button-", "");
+            if(!textBtn){
+                textBtn = btn.className.replace("button-", "");
+            }
+
+            this.functionsBtn(textBtn);
+
+        });
+    });
+}
